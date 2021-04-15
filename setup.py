9adbbf9eb2d694ddc4fcf5a5371528c6d1dcbcd9
everhide <i.tolkachnikov@gmail.com>
2021-04-16 02:21:32 +0300

    [Initial]

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='wallet-one-payments',
+    version='0.1',
+    description='Form wrapper for Wallet One Payments.',
+    url='http://github.com/everhide/wallet-one-payments',
+    author='everhide',
+    author_email='i.tolkachnikov@gmail.com',
+    license='MIT',
+    packages=['wallet-one-payments'],
+    zip_safe=False,
+)
